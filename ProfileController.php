8bec8130c1d3ebee626f5a530572a282f523f55a
m-igarashi <107655755+mmcigrs@users.noreply.github.com>
2022-07-15 02:57:45 +0000

    PHP/Laravel04 kadai

--- ProfileController.php
+++ ProfileController.php
@@ -28,3 +28,11 @@ class ProfileController extends Controller
         return redirect('admin/profile/create');
     }
 }
+
+//admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください
+Route::group(['prefix' => 'admin'],function(){
+    Route::get('profile/create',
+'Admin\ProfileController@add');
+    Route::get('profile/edit',
+'Admin\ProfileController@edit');
+});
