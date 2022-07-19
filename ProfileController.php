f496fd8c66bc5f88d5b02180f4b044d948c73d88
m-igarashi <107655755+mmcigrs@users.noreply.github.com>
2022-07-19 11:46:48 +0000

    PHP/laravel09 kadai

--- ProfileController.php
+++ ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
@@ -13,26 +15,34 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
+        
+        $profile->fill($form);
+        $profile->save();
+
         return redirect('admin/profile/create');
     }
     
     public function edit()
     {
-        return view('admin.profile.create');
+        return view('admin.profile.edit');
     }
     
     public function update()
     {
-        return redirect('admin/profile/create');
+        return redirect('admin/profile/edit');
     }
 }
 
 //admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください
-Route::group(['prefix' => 'admin'],function(){
-    Route::get('profile/create',
-'Admin\ProfileController@add');
-    Route::get('profile/edit',
-'Admin\ProfileController@edit');
-});
+// Route::group(['prefix' => 'admin'],function(){
+//     Route::get('profile/create',
+// 'Admin\ProfileController@add');
+//     Route::get('profile/edit',
+// 'Admin\ProfileController@edit');
+// });
